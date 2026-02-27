6de44dcd7285f447535b1e3604bd6b03d637c911
sophiesam221 <sophiesam221@protonmail.com>
2026-02-27 20:24:37 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Trend Micro Download Already Purchased – The Ultimate Device-Ready Installation Plan"
+html_title = "Instructions to Install Trend Micro Already Purchased on My Computer Using www.trendmicro.com Home Install Portal"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
